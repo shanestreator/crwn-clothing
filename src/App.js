18d7328b524b24fccc4cd517fc98ed4b7ba2ee97
Shane Streator <shanestreator@gmail.com>
2019-12-06 21:58:08 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './homepage.styles.scss'
+
+import Homepage from './homepage.component'
+
+function App() {
+  return (
+    <div>
+       <Homepage />
+    </div>
+  );
+}
+
+export default App;
